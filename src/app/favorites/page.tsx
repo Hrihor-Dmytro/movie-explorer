ec84cf4ea18973cb7f98b12a9635438f8378d561
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2025-04-29 19:44:50 +0200

    Add delite favorites

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { fetchMovieById } from '../../lib/omdb';
 
 export default function FavoritesPage() {
   const [favorites, setFavorites] = useState<string[]>([]);
   const [movies, setMovies] = useState<any[]>([]);
+  const pathname = usePathname();
+
+  const removeFromFavorites = (id: string) => {
+    const updated = favorites.filter((favId) => favId !== id);
+    setFavorites(updated);
+    localStorage.setItem('favorites', JSON.stringify(updated));
+    setMovies((prev) => prev.filter((movie) => movie.imdbID !== id));
+  };
 
   const loadFavoriteMovies = async () => {
     try {
@@ -33,16 +43,43 @@ export default function FavoritesPage() {
 
   return (
     <div className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">⭐ Избранные фильмы</h1>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">⭐ Избранные фильмы</h1>
+        <div className="flex gap-4 items-center">
+          <Link
+            href="/"
+            className={`hover:underline ${pathname === '/' ? 'font-bold underline' : 'text-blue-500'}`}
+          >
+            Главная
+          </Link>
+          <Link
+            href="/favorites"
+            className={`hover:underline ${pathname === '/favorites' ? 'font-bold underline' : 'text-blue-500'}`}
+          >
+            Избранные ⭐
+          </Link>
+        </div>
+      </div>
+
       <ul className="space-y-4">
         {movies.map((movie) => (
           <li key={movie.imdbID} className="border p-4 rounded">
-            <h2 className="text-xl font-semibold">{movie.Title}</h2>
-            <p>{movie.Year}</p>
-            {movie.Poster !== 'N/A' && (
-              <img src={movie.Poster} alt={movie.Title} className="mt-2 w-32" />
-            )}
-          </li>
+          <div className="flex justify-between items-center">
+            <div>
+              <h2 className="text-xl font-semibold">{movie.Title}</h2>
+              <p>{movie.Year}</p>
+            </div>
+            <button
+              onClick={() => removeFromFavorites(movie.imdbID)}
+              className="ml-4 px-2 py-1 bg-red-600 text-white rounded"
+            >
+              Удалить
+            </button>
+          </div>
+          {movie.Poster !== 'N/A' && (
+            <img src={movie.Poster} alt={movie.Title} className="mt-2 w-32" />
+          )}
+        </li>
         ))}
       </ul>
     </div>
